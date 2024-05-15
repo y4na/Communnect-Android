05ab647d10e93715c3575b67fc086706cecb71a5
Jeecoo <johnmark.econar7@gmail.com>
2024-05-15 22:51:45 +0800

    added firebase database (register and login)

---
+++ app/src/main/java/com/example/communnect/HelperClass.java
@@ -0,0 +1,49 @@
+package com.example.communnect;
+
+public class HelperClass {
+
+    String firstname, lastname, email, password;
+
+    public HelperClass(String firstname, String lastname, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return firstname;
+    }
+
+    public void setName(String name) {
+        this.firstname = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public HelperClass() {
+    }
+}
+
