c5618a875a5f2c8b855ea67a542fe3b63eae6432
LAB11_USER ON WS31 <L11Y08W31@citulab.net>
2024-05-16 10:19:45 +0800

    fragments created

---
+++ app/src/main/java/com/example/communnect/DashboardActivity.java
@@ -0,0 +1,50 @@
+package com.example.communnect;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    NewsFragment newsFragment = new NewsFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    FavoritesFragment favoritesFragment = new FavoritesFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, newsFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_news:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, newsFragment).commit();
+                        return true;
+                    case R.id.nav_search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).commit();
+                        return true;
+                    case R.id.nav_favorite:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, favoritesFragment).commit();
+                        return true;
+                    case R.id.nav_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
