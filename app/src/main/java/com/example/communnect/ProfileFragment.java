64192f064c56f73e28451e7b476793db1d1092ec
yanaparagoso <paragosoyana@gmail.com>
2024-05-30 05:22:11 +0800

    read google sign in name from database

--- app/src/main/java/com/example/communnect/ProfileFragment.java
+++ app/src/main/java/com/example/communnect/ProfileFragment.java
@@ -11,6 +11,11 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserInfo;
+
 public class ProfileFragment extends Fragment {
 
     private TextView setTextFullName;
@@ -66,23 +71,35 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        UserProfile userProfile = new UserProfile();
-        Bundle args = getArguments();
-        if (args != null) {
-            String userId = args.getString("userId");
-            if (userId != null) {
-                userProfile.getFullname(userId, new UserProfile.FullNameCallback() {
-                    @Override
-                    public void onCallback(String fullName) {
-                        if (fullName != null) {
-                            setTextFullName.setText(fullName);
-                        } else {
-                            System.out.println("Failed to retrieve full name.");
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            for (UserInfo profile : currentUser.getProviderData()) {
+                if (profile.getProviderId().equals(GoogleAuthProvider.PROVIDER_ID)) {
+                    setTextFullName.setText(currentUser.getDisplayName());
+                    break;
+                } else {
+                    UserProfile userProfile = new UserProfile();
+                    Bundle args = getArguments();
+                    if (args != null) {
+                        String userId = args.getString("userId");
+                        if (userId != null) {
+                            userProfile.getFullname(userId, new UserProfile.FullNameCallback() {
+                                @Override
+                                public void onCallback(String fullName) {
+                                    if (fullName != null) {
+                                        setTextFullName.setText(fullName);
+                                    } else {
+                                        System.out.println("Failed to retrieve full name.");
+                                    }
+                                }
+                            });
                         }
                     }
-                });
+                }
             }
         }
+
+
 
         return view;
     }
